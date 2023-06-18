abb0d18fb9243fb4abf02702a9c942b541eaa904
WaterKatPC <waterkat@aonyxlimited.com>
2023-06-18 00:57:08 -0700

    Created Subscription Class
    Subscription class is a simple class that creates an easy to manage subscriptor pattern

---
+++ src/js/subscription-methods/subscription.js
@@ -0,0 +1,43 @@
+class Subscription {
+    constructor() {
+        this.functions = [];
+    }
+
+    #newResponse(_success) {
+        const response = {};
+        response.successful = _success;
+        response.then = (_function) => { if (!response.successful) return; _function(); return response; }
+        response.catch = (_function) => { if (response.successful) return; _function(); return response; }
+        return response;
+    }
+
+    subscribe(_function) {
+        if (typeof _function !== 'function') {
+            return this.#newResponse(false);
+        }
+
+        if (this.functions.includes(_function)) {
+            return this.#newResponse(false);
+        }
+
+        this.functions.push(_function);
+        return this.#newResponse(true);
+    }
+
+    unsubscribe(_function) {
+        if (typeof _function !== 'function') {
+            return this.#newResponse(false);
+        }
+
+        if (!this.functions.includes(_function)) {
+            return this.#newResponse(false);
+        }
+
+        const functionIndex = this.functions.indexOf(_function);
+        if (functionIndex !== -1) {
+            this.functions.splice(functionIndex, 1);
+        }
+    }
+}
+
+module.exports = Subscription;
