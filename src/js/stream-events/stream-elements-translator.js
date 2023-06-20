3fba85b3bd8f9a32d629622503e045722d79b2bd
WaterKatPC <waterkat@aonyxlimited.com>
2023-06-19 18:02:52 -0700

    Changing event process management

--- src/js/stream-events/stream-elements-translator.js
+++ src/js/stream-events/stream-elements-translator.js
@@ -1,7 +1,128 @@
 const StreamElements = require('./stream-elements-listener.js');
 
+class StreamEventProcessor {
+    static #stream_event = {
+        username: '',
+        permissions: 'chatter',
+        type: '',
+        message: '',
+        emotes: [],
+        follow: {
+        },
+        sub: {
+            length: 0,
+        },
+        gift: {
+            sender: '',
+            count: 1,
+            isInitial: '',
+            isGifted: '',
+            isBulk: '',
+        },
+        raid: {
+            count: 0,
+        },
+        cheer: {
+            amount: 0,
+        },
+        chat: {
+        },
+        command: {
+            identifier: '',
+            group: '',
+            request: '',
+            args: '',
+        }
+    }
+
+    static ProcessStreamElementEvent(_data) {
+        const streamEvent = Object.create(StreamEventProcessor.#stream_event);
+        //const streamEvent = StreamEventProcessor.#stream_event;
+        //temp
+        const permissions = {
+            streamer : [],
+            moderator : [],
+            vip : [],
+            subscriber : [],
+        };
+
+        //temp
+        const updatePermissions = () => {
+            if (!_data || !_data.event || !_data.event.channel || !_data.event.data || !_data.event.data.tags)
+                return;
+            if (_data.event.data.nick == _data.event.data.channel && !permissions.streamer.includes(_data.event.data.nick)){
+                permissions.streamer.push(_data.event.data.nick);
+                return;
+            }
+            if (_data.event.data.tags.mod === '1' || !permissions.moderator.includes(_data.event.data.nick)){
+                permissions.moderator.push(_data.event.data.nick);
+                return;
+            }
+            if (_data.event.data.tags.vip === '1' || !permissions.vip.includes(_data.event.data.nick)){
+                permissions.vip.push(_data.event.data.nick);
+                return;
+            }
+            if (_data.event.data.tags.subscriber === '1' || !permissions.subscriber.includes(_data.event.data.nick)){
+                permissions.subscriber.push(_data.event.data.nick);
+                return;
+            }
+        };
+        updatePermissions();
+
+        //username
+        const getUsername = () => {
+            let response = '';
+            
+            if (!_data || !_data.event || !_data.event.name)
+                return response;
+            response = _data.event.name.toLowerCase()
+
+            if (!_data.event.data){
+                return response;
+            }
+            if (!_data.event.data.nick){
+                return response;
+            }
+            return _data.event.data.nick.toLowerCase();
+        }
+        streamEvent.username = getUsername();
+
+        //permissions
+        const getPermissions = () => {
+            const defaultResponse = 'chatter';
+
+            const permissionTypes = Object.keys(permissions);
+            permissionTypes.forEach((permissionType) => {
+                if (permissions[permissionType].includes(streamEvent.username)){
+                    return permissionType;
+                }
+            });
+
+            return defaultResponse;
+        }
+        streamEvent.permissions = getPermissions();
+
+        //type
+        if (_data.key !== 'subscriber') {
+            streamEvent.type = _data.key;
+        } else {
+            if (_data.event.bulkGifted || _data.event.isCommunityGift) {
+                streamEvent.type = 'gift-single';
+            } else if (_data.event.gifted) {
+                streamEvent.type = 'gift-bomb';
+            } else {
+                streamEvent.type = 'sub';
+            }
+        }
+
+        return streamEvent;
+    }
+}
+
 function translate(_key, _event) {
-    console.log(`translate SE_Event: ${_key} `, _event);
+    const processedEvent = StreamEventProcessor.ProcessStreamElementEvent({key: _key, event: _event});
+    console.log('Processed Event: ', processedEvent);
+//    console.log(`translate SE_Event: ${_key} `, _event);
 
     let data = {
         type: _key,
@@ -53,8 +174,8 @@ function translate(_key, _event) {
 
         _event.data.emotes.forEach(element => {
             const emote = {
-                type : element.type,
-                name : element.name,
+                type: element.type,
+                name: element.name,
             }
             data.emotes.push(emote);
         })
