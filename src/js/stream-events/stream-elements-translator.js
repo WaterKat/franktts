014e9e8f72c820a724208611d2afa0cdd0d97a32
WaterKatPC <waterkat@aonyxlimited.com>
2023-06-20 19:58:59 -0700

    Finished new StreamEvent Object Creation

--- src/js/stream-events/stream-elements-translator.js
+++ src/js/stream-events/stream-elements-translator.js
@@ -32,58 +32,63 @@ class StreamEventProcessor {
             group: '',
             request: '',
             args: '',
+            isValid: false,
         }
     }
 
     static #updatePermissions(_data, _permissions) {
-        if (!_data || !_data.event || !_data.event.channel || !_data.event.data || !_data.event.data.tags)
+        const username = StreamEventProcessor.#getUsername(_data);
+
+        if (!_permissions)
             return;
-        if (_data.event.data.nick == _data.event.data.channel && !_permissions.streamer.includes(_data.event.data.nick)) {
-            _permissions.streamer.push(_data.event.data.nick);
+
+        if (!_data || !_data.event || !_data.event.data)
             return;
-        }
-        if (_data.event.data.tags.mod === '1' || !_permissions.moderator.includes(_data.event.data.nick)) {
-            _permissions.moderator.push(_data.event.data.nick);
+
+        if (username == _data.event.data.channel && !_permissions.streamer.includes(username))
+            _permissions.streamer.push(username);
+
+        if (!_data.event.data.tags)
             return;
-        }
-        if (_data.event.data.tags.vip === '1' || !_permissions.vip.includes(_data.event.data.nick)) {
-            _permissions.vip.push(_data.event.data.nick);
-            return;
-        }
-        if (_data.event.data.tags.subscriber === '1' || !_permissions.subscriber.includes(_data.event.data.nick)) {
-            _permissions.subscriber.push(_data.event.data.nick);
-            return;
-        }
+
+        if (_data.event.data.tags.mod === '1' && !_permissions.moderator.includes(username))
+            _permissions.moderator.push(username);
+
+
+        if (_data.event.data.tags.vip === '1' && !_permissions.vip.includes(username))
+            _permissions.vip.push(username);
+
+        if (_data.event.data.tags.subscriber === '1' && !_permissions.subscriber.includes(username))
+            _permissions.subscriber.push(username);
+
     };
 
     static #getUsername(_data) {
         let response = '';
 
-        if (!_data || !_data.event || !_data.event.name)
+        if (!_data || !_data.event)
             return response;
-        response = _data.event.name.toLowerCase()
 
-        if (!_data.event.data) {
-            return response;
-        }
-        if (!_data.event.data.nick) {
-            return response;
+        response = _data.event.name || response;
+
+        if (_data.event.data) {
+            response = _data.event.data.nick || response;
         }
 
-        return _data.event.data.nick.toLowerCase();
+        return response.toLowerCase();
     }
 
     static #getPermissions(_username, _permissions) {
-        const defaultResponse = 'chatter';
+        const response = ['chatter'];
 
         const permissionTypes = Object.keys(_permissions);
         permissionTypes.forEach((permissionType) => {
             if (_permissions[permissionType].includes(_username)) {
-                return permissionType;
+                response.push(permissionType);
             }
         });
 
-        return defaultResponse;
+        return response;
     }
 
     static #getType(_data) {
@@ -106,7 +111,7 @@ class StreamEventProcessor {
         }
     }
 
-    static #getMessage(_data){
+    static #getMessage(_data) {
         let response = '';
 
         if (!_data || !_data.event)
@@ -120,20 +125,20 @@ class StreamEventProcessor {
         return response;
     }
 
-    static #getEmotes(_data){
+    static #getEmotes(_data) {
         let response = [];
 
         if (!_data || !_data.event || !_data.event.data || !_data.event.data.emotes)
             return response;
 
         _data.event.data.emotes.forEach(emote => {
-            response.push({type: emote.type, name: emote.name});
+            response.push({ type: emote.type, name: emote.name });
         });
 
         return response;
     }
 
-    static #getSub(_data){
+    static #getSub(_data) {
         const response = {
             length: 0,
         };
@@ -146,23 +151,110 @@ class StreamEventProcessor {
         return response;
     }
 
-    static #getGift(_data){
+    static #getGift(_data) {
         const response = {
             sender: '',
             count: 1,
             isInitial: false,
-            isGifted: true,
+            isGifted: false,
             isBulk: false,
         };
 
-        if (!_data || !_data.event || !_data.event.sender)
+        if (!_data || !_data.event)
             return response;
-        
-        response.sender = _data.event.sender;
-        response.count = _data.event.amount || 1;
-        response.isInitial = _data.event.bulkGifted || false;
-        response.isGifted = _data.event.gifted || false;
-        response.isBulk = _data.event.isCommunityGift || false;
+
+        response.sender = _data.event.sender || response.sender;
+        response.count = _data.event.amount || response.count;
+        response.isInitial = _data.event.bulkGifted || response.isInitial;
+        response.isGifted = _data.event.gifted || response.isGifted;
+        response.isBulk = _data.event.isCommunityGift || response.isBulk;
+
+        return response;
+    }
+
+    static #getRaid(_data) {
+        const response = {
+            count: 0,
+        };
+
+        if (!_data || !_data.event)
+            return response;
+
+        response.count = _data.event.amount || response.count;
+
+        return response;
+    }
+
+    static #getCheer(_data) {
+        const response = {
+            amount: 0,
+        };
+
+        if (!_data || !_data.event)
+            return response;
+
+        response.amount = _data.event.amount || response.amount;
+
+        return response;
+    }
+
+    static #getCommand(_data) {
+        const response = {
+            identifier: '',
+            group: '',
+            request: '',
+            args: '',
+            isValid: false,
+        }
+
+        let rawMessage = '';
+
+        if (!_data || !_data.event)
+            return rawMessage;
+
+        rawMessage = _data.event.message || rawMessage;
+
+        if (_data.event.data)
+            rawMessage = _data.event.data.text || rawMessage;
+
+        rawMessage = rawMessage.trim();
+
+        //4 Is a good minimum length as it allows the minimum requirements '!a b'
+        const minimumCommandLength = 4;
+        if (rawMessage.length <= minimumCommandLength)
+            return response;
+
+        const isAlphaNumericReg = /^[a-zA-Z0-9]+$/;
+        const isWhitespaceReg = /\s+/;
+
+        //Command Identifier ex: '!'
+        const firstChar = rawMessage[0];
+        if (isAlphaNumericReg.test(firstChar)) {
+            return response;
+        }
+
+        const wordArray = rawMessage.substring(1).split(isWhitespaceReg).filter(word => word !== "");
+
+        //Ensure there are atleast two words;
+        if (wordArray.length < 2)
+            return response;
+
+        const firstWord = wordArray[0];
+        const secondWord = wordArray[1];
+
+        //AT THIS POINT THIS IS A VALID COMMAND
+        response.identifier = firstChar || response.identifier;
+        response.group = firstWord || response.group;
+        response.request = secondWord || response.request;
+        response.isValid = true;
+
+        //Grab the rest of the message
+        const argsIndex = rawMessage.indexOf(secondWord) + secondWord.length;
+        if (argsIndex >= rawMessage.length)
+            return response;
+
+        const restOfMessage = rawMessage.substring(argsIndex);
+        response.args = restOfMessage || response.args;
 
         return response;
     }
@@ -180,6 +272,7 @@ class StreamEventProcessor {
 
         //SHOULD BE MOVED OUTSIDE
         StreamEventProcessor.#updatePermissions(_data, permissions);
+        console.log(permissions);
 
         //username
         streamEvent.username = StreamEventProcessor.#getUsername(_data);
@@ -197,12 +290,28 @@ class StreamEventProcessor {
         streamEvent.emote = StreamEventProcessor.#getEmotes(_data);
 
         //follow
+        //        streamEvent.follow = StreamEventProcessor.#getFollow(_data);
 
         //sub
         streamEvent.sub = StreamEventProcessor.#getSub(_data);
 
         //gift
         streamEvent.gift = StreamEventProcessor.#getGift(_data);
+
+        //raid
+        streamEvent.raid = StreamEventProcessor.#getRaid(_data);
+
+        //cheer
+        streamEvent.cheer = StreamEventProcessor.#getCheer(_data);
+
+        //chat
+        //        streamEvent.chat = StreamEventProcessor.#getChat(_data);
+
+        //command
+        streamEvent.command = StreamEventProcessor.#getCommand(_data);
+        if (streamEvent.command.isValid){
+            streamEvent.type = 'command'
+        }
 
         return streamEvent;
     }
