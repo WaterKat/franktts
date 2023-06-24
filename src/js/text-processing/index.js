d8f2f727801b058025213f6f6001d1facbe2807e
WaterKatPC <waterkat@aonyxlimited.com>
2023-06-24 16:07:16 -0700

    Renaming filterALL function to lowercase All

--- src/js/text-processing/index.js
+++ src/js/text-processing/index.js
@@ -21,12 +21,12 @@ class TTSFilter{
         return this.badEmoteFilter.filter(_text, _emotes);
     }
 
-    filterALL(_text, _emotes = [], _blacklist = []){
+    filterAll(_text, _emotes = [], _blacklist = []){
         let response = _text;
 
-        response = this.filterBadWords(response);
         response = this.filterBadChars(response);
         response = this.filterBadEmotes(response, _emotes);
+        response = this.filterBadWords(response);
 
         if (_blacklist.length > 0){
             response = new WordFilter(_blacklist, true, '').filter(response);
