84d04f610750f7d3178713216a42e26ba2d71961
WaterKatPC <waterkat@aonyxlimited.com>
2023-06-25 13:15:53 -0700

    Added reference to new modules

--- src/js/stream-events/index.js
+++ src/js/stream-events/index.js
@@ -1,5 +1,28 @@
+const AonyxEventListener = require('./aonyx-events/index.js').AonyxEventListener;
+
+const StreamElementsEventListener = require('./stream-elements/index.js').StreamElementsEventListener;
+const StreamElementsEventTranslator = require('./aonyx-events/stream-elements-translator.js');
+
+function GetStreamEventListener(_permissions) {
+    const aonyxEventListener = new AonyxEventListener();
+    const streamelementsEventListener = new StreamElementsEventListener();
+    const streamelementsEventTranslator = new StreamElementsEventTranslator(_permissions);
+
+
+    streamelementsEventListener.activeSubscription.subscribe(
+        (_streamElementsEvent) => {
+            aonyxEventListener.passthrough(
+                streamelementsEventTranslator.processStreamElementEvent(_streamelementsEvent)
+            );
+        });
+
+
+    return aonyxEventListener;
+}
+
 
 module.exports = {
-    StreamElementsEvents : require('./stream-elements-listener.js'),
-    StreamEventProcessor : require('./stream-elements-translator.js'),
+    GetStreamEventListener : GetStreamEventListener,
+    StreamElementsEvents: require('./stream-elements-listener.js'),
+    StreamEventProcessor: require('./stream-elements-translator.js'),
 }
