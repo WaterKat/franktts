b15ff17ce998fdd16cc10e7919de5400c451b5e4
WaterKatPC <waterkat@aonyxlimited.com>
2023-06-28 23:16:11 -0700

    Adding Vtuber integration

---
+++ src/js/vtuber/vtuber.js
@@ -0,0 +1,80 @@
+// ES Modules
+import { ApiClient } from "vtubestudio";
+import { WebSocket } from "ws";
+import { icon } from "./config/icon.js";
+import fs from 'fs';
+
+function lerp(a, b, alpha) {
+    return a + alpha * (b - a);
+}
+function clamp(min, max, val) {
+    return Math.min(Math.max(val, min), max);
+}
+function randomNormal() {
+    return (Math.random() * 2) - 1;
+}
+
+function setAuthToken(authenticationToken) {
+    // store the authentication token somewhere
+    fs.writeFileSync("./auth-token.txt", authenticationToken, {
+        encoding: "utf-8",
+    });
+}
+
+function getAuthToken() {
+    // retrieve the stored authentication token
+    return fs.readFileSync("./auth-token.txt", "utf-8");
+}
+
+const options = {
+    authTokenGetter: getAuthToken,
+    authTokenSetter: setAuthToken,
+    pluginName: "FrankTTS",
+    pluginDeveloper: "AonyxLimited",
+    pluginIcon: icon,
+    webSocketFactory: (url) => new WebSocket(url),
+    // Optionally set the URL or port to connect to VTube Studio at; defaults are as below:
+
+    //port: 25565,
+    //url: `ws://localhost:${port}`,
+    url: `ws://10.0.0.10:8001`,
+};
+
+const apiClient = new ApiClient(options);
+
+
+apiClient.on("connect", async () => {
+    VTuber.ready = true;
+});
+
+
+class VTuber {
+    static ready = false;
+    constructor() {
+        this.mouth = 0;
+        this.weight = 0.5
+
+    }
+
+    update_amplitude(_amplitude) {
+        if (!VTuber.ready)
+            return;
+
+        this.mouth = _amplitude;
+
+        apiClient.injectParameterData(
+            {
+                parameterValues: [
+                    {
+                        id: 'MouthOpen',
+                        value: this.mouth,
+                        weight: this.weight,
+                    },
+                ]
+            },
+        );
+
+    }
+}
+
+module.exports = VTuber
